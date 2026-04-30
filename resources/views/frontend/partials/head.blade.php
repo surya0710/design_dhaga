b7e38a37a5ce2b45f6260d8a484fc7a9c4588816
Surya <157574400+0710surya@users.noreply.github.com>
2026-04-30 22:05:52 +0530

    Added fix for zoom

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 <!-- PRIMARY SEO -->
