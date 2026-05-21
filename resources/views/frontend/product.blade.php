03d1a2fb7f20d5c1a657b7fe90c5e7701f2893c8
Surya <157574400+0710surya@users.noreply.github.com>
2026-05-21 23:17:45 +0530

    Fixed product page

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -602,13 +602,13 @@
                                 <img src="{{ Storage::url($product->image) }}" loading="lazy" class="card-img-top object-fit-cover" alt="{{ $product->name }}" />
                             </div>
                             <div class="card-body">
-                                <p class="mt-2 text-left">{{ $product->name }}</p>
+                                <p class="mt-2 mb-0 text-left">{{ $product->name }}</p>
                                 @if ($product->sale_price)
-                                    <span class="text-black">₹ {{ number_format($product->sale_price, 0) }}</span>
-                                    <span class="text-decoration-line-through text-muted small ms-2">
-                                        ₹ {{ number_format($product->regular_price, 0) }}
+                                    <span class="text-black text-bold">₹{{ number_format($product->sale_price, 0) }}</span>
+                                    <span class="text-decoration-line-through text-muted small text-light">
+                                        ₹{{ number_format($product->regular_price, 0) }}
                                     </span>
-                                    <span class="text-maroon small ms-2 fw-semibold">
+                                    <span class="text-maroon small">
                                         Save {{ number_format((1 - ($product->sale_price / $product->regular_price)) * 100, 0) }}%
                                     </span>
                                 @else
